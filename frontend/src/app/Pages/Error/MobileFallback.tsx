02fa0217a2e1f30c35c71a63282d4432bb42dfb5
Tomás Alberdi <tomas.alberdi.2001@gmail.com>
2025-07-17 19:24:16 -0300

    added mobile fallback and not found components

--- frontend/src/app/Pages/Error/MobileFallback.tsx
+++ frontend/src/app/Pages/Error/MobileFallback.tsx
@@ -1,10 +1,10 @@
-const NotFound = () => {
+const MobileFallback = () => {
   return (
     <div className="page-full-h flex justify-center items-center">
       <h1 className="text-4xl text-center font-bold alternate-font">
-        Página no encontrada.
+        Página no disponible en dispositivos móviles.
       </h1>
     </div>
   )
 }
-export default NotFound
+export default MobileFallback
--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,3 +1,4 @@
+import MobileFallback from "@/app/Pages/Error/MobileFallback";
 import { AppSidebar } from "@/components/app-sidebar";
 import { Separator } from "@/components/ui/separator";
 import {
@@ -5,9 +6,16 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
+import { useIsMobile } from "@/hooks/use-mobile";
 import { Outlet } from "react-router-dom";
 
 export default function Page() {
+  const isMobile = useIsMobile();
+
+  if (isMobile) {
+    return <MobileFallback />;
+  }
+
   return (
     <SidebarProvider>
       <AppSidebar />
