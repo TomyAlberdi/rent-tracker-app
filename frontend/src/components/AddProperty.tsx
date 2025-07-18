f5f1e91fb153708da7752ded32f5164ca7ec1d7b
Tomás Alberdi <tomas.alberdi.2001@gmail.com>
2025-07-17 23:16:17 -0300

    created add property component

--- frontend/src/components/AddProperty.tsx
+++ frontend/src/components/AddProperty.tsx
@@ -4,11 +4,67 @@ import {
   DialogContent,
   DialogHeader,
   DialogTitle,
-  DialogTrigger
+  DialogTrigger,
 } from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { CirclePlus } from "lucide-react";
+import { useForm } from "react-hook-form";
+import z from "zod";
+
+const groups = [
+  {
+    id: 1,
+    name: "Grupo 1",
+  },
+  {
+    id: 2,
+    name: "Grupo 2",
+  },
+  {
+    id: 3,
+    name: "Grupo 3",
+  },
+];
+
+const formSchema = z.object({
+  name: z.string().min(1, "El nombre es obligatorio"),
+  description: z.string().optional(),
+  type: z.enum(["GROUPED", "INDIVIDUAL"]),
+  groupId: z.string().optional().nullable(),
+});
 
 const AddProperty = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      description: "",
+      type: "INDIVIDUAL",
+      groupId: null,
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+    //TODO: Format group ID to number
+  };
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -19,10 +75,74 @@ const AddProperty = () => {
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>
-            Agregar Propiedad
-          </DialogTitle>
+          <DialogTitle>Agregar Propiedad</DialogTitle>
         </DialogHeader>
+        <div>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="flex flex-col gap-4"
+            >
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Nombre</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Descripción</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="groupId"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Grupo</FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={undefined}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Seleccione un grupo (opcional)" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {groups.map((group) => (
+                          <SelectItem key={group.id} value={group.id.toString()}>
+                            {group.name}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button>
+                <CirclePlus />
+                Crear
+              </Button>
+            </form>
+          </Form>
+        </div>
       </DialogContent>
     </Dialog>
   );
