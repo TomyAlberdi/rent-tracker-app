1f07d107ca13b80ae3b0d103ed142ddbe50cf3e9
Tomás Alberdi <66546046+TomyAlberdi@users.noreply.github.com>
2025-07-18 14:29:15 -0300

    added sonner

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
+import { Toaster } from "@/components/ui/sonner";
 import CustomRouter from "@/lib/routes";
 
 function App() {
   return (
     <div>
       <CustomRouter />
+      <Toaster />
     </div>
   );
 }
