16eae90643a163e108b56b5233f09c1ae948e643
Tomás Alberdi <66546046+TomyAlberdi@users.noreply.github.com>
2025-07-22 16:43:10 -0300

    simplified group nav sidebar

--- frontend/src/components/nav-group.tsx
+++ frontend/src/components/nav-group.tsx
@@ -1,62 +1,36 @@
 "use client";
-
-import { ChevronRight } from "lucide-react";
-
 import {
-  Collapsible,
-  CollapsibleContent,
-  CollapsibleTrigger,
+  Collapsible
 } from "@/components/ui/collapsible";
 import {
   SidebarGroup,
   SidebarGroupLabel,
   SidebarMenu,
-  SidebarMenuAction,
   SidebarMenuButton,
-  SidebarMenuItem,
-  SidebarMenuSub,
-  SidebarMenuSubButton,
-  SidebarMenuSubItem,
+  SidebarMenuItem
 } from "@/components/ui/sidebar";
 import type { GroupListingItem } from "@/lib/interfaces";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export function NavGroup({ items }: { items: GroupListingItem[] }) {
+  const navigate = useNavigate();
+
   return (
     <SidebarGroup>
       <SidebarGroupLabel>Grupos</SidebarGroupLabel>
       <SidebarMenu>
         {items.map((item) => (
-          <Collapsible key={item.id} asChild defaultOpen={true}>
+          <Collapsible
+            key={item.id}
+            asChild
+            defaultOpen={true}
+            className="cursor-pointer rounded-md hover:bg-sidebar-accent hover:text-sidebar-accent-foreground"
+            onClick={() => navigate(`/group/${item.id}`)}
+          >
             <SidebarMenuItem>
               <SidebarMenuButton asChild tooltip={item.name}>
-                <Link to={`/group/${item.id}`}>
-                  <span>{item.name}</span>
-                </Link>
+                  <span className="text-sm p-2">{item.name}</span>
               </SidebarMenuButton>
-              {item.properties?.length ? (
-                <>
-                  <CollapsibleTrigger asChild className="cursor-pointer">
-                    <SidebarMenuAction className="data-[state=open]:rotate-90">
-                      <ChevronRight />
-                      <span className="sr-only">Toggle</span>
-                    </SidebarMenuAction>
-                  </CollapsibleTrigger>
-                  <CollapsibleContent>
-                    <SidebarMenuSub>
-                      {item.properties?.map((subItem) => (
-                        <SidebarMenuSubItem key={subItem.id}>
-                          <SidebarMenuSubButton asChild>
-                            <Link to="/">
-                              <span>{subItem.name}</span>
-                            </Link>
-                          </SidebarMenuSubButton>
-                        </SidebarMenuSubItem>
-                      ))}
-                    </SidebarMenuSub>
-                  </CollapsibleContent>
-                </>
-              ) : null}
             </SidebarMenuItem>
           </Collapsible>
         ))}
