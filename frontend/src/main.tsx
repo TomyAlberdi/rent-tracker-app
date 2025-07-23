e93eb4cab839a57c1726524a5c944df89529664b
Tomás Alberdi <66546046+TomyAlberdi@users.noreply.github.com>
2025-07-23 13:37:35 -0300

    removed strict mode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,22 +4,19 @@ import PropertyContextComponent from "@/context/PropertyContextComponent";
 import RecordContextComponent from "@/context/RecordContextComponent";
 import "@/index.css";
 import { ThemeProvider } from "@/theme/theme-provider";
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <GroupContextComponent>
-        <PropertyContextComponent>
-          <RecordContextComponent>
-            <BrowserRouter>
-              <App />
-            </BrowserRouter>
-          </RecordContextComponent>
-        </PropertyContextComponent>
-      </GroupContextComponent>
-    </ThemeProvider>
-  </StrictMode>
+  <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <GroupContextComponent>
+      <PropertyContextComponent>
+        <RecordContextComponent>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </RecordContextComponent>
+      </PropertyContextComponent>
+    </GroupContextComponent>
+  </ThemeProvider>
 );
