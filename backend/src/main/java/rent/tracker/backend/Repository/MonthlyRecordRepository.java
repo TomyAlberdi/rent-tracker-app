cbb87d325031fcae7ff882c05f36a13467c8b254
Tomás Alberdi <66546046+TomyAlberdi@users.noreply.github.com>
2025-07-26 15:04:27 -0300

    added annotation

--- backend/src/main/java/rent/tracker/backend/Repository/MonthlyRecordRepository.java
+++ backend/src/main/java/rent/tracker/backend/Repository/MonthlyRecordRepository.java
@@ -1,12 +1,14 @@
 package rent.tracker.backend.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import rent.tracker.backend.Entity.MonthlyRecord;
 import rent.tracker.backend.Entity.Property;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface MonthlyRecordRepository extends JpaRepository<MonthlyRecord, Long> {
     Optional<MonthlyRecord> findByPropertyIdAndMonthAndYear(Long propertyId, Integer month, Integer year);
     List<MonthlyRecord> findByPropertyAndYear(Property property, int year);
