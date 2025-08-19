3bb1cd0c389b6807a1dbd770c10ce0e11309d366
Tomás Alberdi <66546046+TomyAlberdi@users.noreply.github.com>
2025-08-19 17:03:19 -0300

    implemented new endpoints in record context

--- frontend/src/context/RecordContext.tsx
+++ frontend/src/context/RecordContext.tsx
@@ -1,10 +1,25 @@
-import type { CreateRecordDTO, PropertyType, Record } from "@/lib/interfaces";
+import type {
+  CreateRecordDTO,
+  MonthlySummaryRecordDTO,
+  ParentSummaryRecordDTO,
+  PropertyType,
+  Record,
+} from "@/lib/interfaces";
 import { createContext } from "react";
 
 export interface RecordContextType {
-  getRecords: (type: PropertyType, parentId: string, year: number) => Promise<Record[]>;
+  getRecords: (
+    type: PropertyType,
+    parentId: string,
+    year: number
+  ) => Promise<Record[]>;
   saveRecord: (record: CreateRecordDTO) => Promise<void>;
-  deleteRecord: (id: string) => Promise<void>; 
+  deleteRecord: (id: string) => Promise<void>;
+  getMonthlySummary: (year: number) => Promise<MonthlySummaryRecordDTO[]>;
+  getParentSummary: (
+    year: number,
+    parentType: PropertyType
+  ) => Promise<ParentSummaryRecordDTO[]>;
 }
 
 export const RecordContext = createContext<RecordContextType | null>(null);
--- frontend/src/lib/interfaces.tsx
+++ frontend/src/lib/interfaces.tsx
@@ -63,6 +63,22 @@ export interface Transaction {
   temporalId?: string;
 }
 
+export interface MonthlySummaryRecordDTO {
+  month: number;
+  year: number;
+  totalIncome: number;
+  totalExpenses: number;
+  netIncome: number;
+}
+
+export interface ParentSummaryRecordDTO {
+  year: number;
+  netIncome: number;
+  parentType: PropertyType;
+  parentId: string;
+  parentName: string;
+}
+
 export type PropertyType = 'INDIVIDUAL' | 'GROUPED';
 
 export type ExpenseType = 'INCOME' | 'EXPENSE';
